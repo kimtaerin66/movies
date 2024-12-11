05838eb46f8dceda0021fce6e73a5d1038be856f
taerin <yeonjikim66@gmail.com>
2024-12-11 18:27:26 +0900

    영화 랭킹 테이블 개발중

--- movies/src/pages/RankMovies.jsx
+++ movies/src/pages/RankMovies.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import styled from "styled-components";
+import requestForRank from "../config/AxiosForRank.js";
+import moment from "moment/moment.js";
+import request from "../config/Axios.js";
 
 
 const Wrap = styled.div`
@@ -7,7 +10,7 @@ const Wrap = styled.div`
     width: 100%;
     padding: 50px;
     min-width: 1200px;
-   box-sizing: border-box;
+    box-sizing: border-box;
 `;
 const Title = styled.h2`
     width: 90%;
@@ -17,22 +20,147 @@ const Title = styled.h2`
     padding-bottom: 10px;
     border-bottom: 1px solid #e1e4e8;
 `;
+const Number = styled.div`
+    -webkit-text-stroke-color: #dadada;
+    -webkit-text-stroke-width: 1px;
+    color: transparent;
+    font-size: 160px;
+    display: flex;
+`;
 const Ul = styled.ul`
+    display: flex;
+    width: 90%;
+    justify-content: center;
+    margin: 50px 0;
+
 `
 const Li = styled.li`
-    margin-bottom: 20px;
+    display: flex;
+    margin-right: 50px;
+
+    &:last-child {
+        margin-right: 0;
+    }
+`;
+
+const Poster = styled.div`
+        //background-image: url(${(props) => props.bg});
+    background-color: #888888;
+    background-size: cover;
+    background-position: top center;
+    cursor: pointer;
+    position: relative;
+    width: 180px;
+    height: 220px;
+    font-size: 16px;
+
+`;
+const Table = styled.table`
+    color: #fff;
+    width: 700px;
+    border: 1px solid #888;
+    text-align: center;
+  
+`;
+
+const Tr = styled.tr`
+    border-bottom: 1px solid #888;
+`;
+const Th = styled.th`
+    font-weight: 500;
+    padding: 15px 25px;
+`;
+const Td = styled.td`
+    padding: 16px 25px;
+    vertical-align: middle;
+    line-height: 22px;
 `;
 
 
-
 function RankMovies() {
-    return <Wrap>
-        <Title>이번주 영화 순위</Title>
+    const VITE_KOFIC_KEY = import.meta.env.VITE_KOFIC_KEY;
+    const [poster, setPoster] = useState([]);
+    const [todayRow, setTodayRow] = useState([]);
+
+    const thead = ["순위", "영화명", "개봉일", "당일 관객수", "누적 관객수"];
+
+    const getRank = () => {
+        (async () => {
+            try {
+                //어제날짜구하기
+                const date = moment(new Date()).subtract(1, "day").format("YYYYMMDD");
+                const res = await requestForRank.get(``, {
+                    params: {
+                        key: VITE_KOFIC_KEY,
+                        targetDt: date,
+                        itemPerPage: "10",
+                    }
+                });
+                setTodayRow(res.data.boxOfficeResult.dailyBoxOfficeList.slice(0, 5));
+            } catch (e) {
+                console.error(e);
+            }
+        })();
+    };
+
+    const getPoster = (row) => {
+        const SERVICE_KEY = import.meta.env.VITE_SERVICE_KEY;
+        const updatePoster = [];
+        row.map((el) => {
+            (async () => {
+                const res = await request.get(``, {
+                    params: {
+                        collection: "kmdb_new2",
+                        detail: "Y", //포스터
+                        ServiceKey: SERVICE_KEY,
+                        listCount: 1,
+                        title: el.movieNm,
+                        releaseDts: el.openDt.replaceAll("-", "")
+                    }
+                });
+                if (res) {
+                    const url = res.data.Data[0].Result[0].posters.slice(0, 60);
+                    updatePoster[el.rank] = [el.movieNm, url];
+                    setPoster(updatePoster);
+                    console.log(updatePoster)
+
+                }
+            })();
+        })
+    };
+
+    useLayoutEffect(() => {
+        getRank();
+    }, [])
+
+
+    useEffect(() => {
+        console.log(todayRow)
+    }, [todayRow])
+
+    return (<Wrap>
+            <Title>이번주 영화 순위</Title>
             <Ul>
-                <Li>
-                </Li>
+                {todayRow.map((movie, index) =>
+                    <Li key={movie.rank}>
+                        <Number>
+                            {movie.rank}
+                        </Number>
+                        <Poster/>
+                    </Li>
+                )}
             </Ul>
-    </Wrap>
+            <Table>
+                <Tr>  {thead.map((el, index) => (
+                    <Th key={index}>{el}</Th>
+                ))}
+                </Tr>
+                    {todayRow.map((el)=>(
+                        <Tr>  <Td>{el.rank}</Td> <Td>{el.movieNm}</Td> <Td>{el.openDt}</Td><Td>{el.audiCnt}</Td><Td>{el.audiAcc}</Td> </Tr>
+                    ))}
+            </Table>
+        </Wrap>
+    )
 }
 
 export default RankMovies;
