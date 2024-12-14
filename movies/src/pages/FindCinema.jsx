b2b9f1716321085080e07b5104ca58d6d36b7437
taerin <yeonjikim66@gmail.com>
2024-12-14 17:36:17 +0900

    카카오 맵 추가

--- movies/src/pages/FindCinema.jsx
+++ movies/src/pages/FindCinema.jsx
@@ -1,37 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import {Map} from "react-kakao-maps-sdk";
 
 
 const Wrap = styled.div`
-    margin-top: 70px;
-    width: 100%;
     padding: 50px;
-    min-width: 1200px;
-       box-sizing: border-box;
+    width: 1400px;
+    margin: 0 auto;
+    box-sizing: border-box;
 `;
 const Title = styled.h2`
-    width: 90%;
+    margin-top: 70px;
     font-size: 22px;
     font-weight: 700;
     color: #e1e4e8;
     padding-bottom: 10px;
     border-bottom: 1px solid #e1e4e8;
+    margin-bottom: 40px;
 `;
-const Ul = styled.ul`
-`
-const Li = styled.li`
-    margin-bottom: 20px;
-`;
-
-
 
 function FindCinema() {
     return <Wrap>
         <Title>내 주변 영화관찾기</Title>
-            <Ul>
-                <Li>
-                </Li>
-            </Ul>
+        <Map
+            center={{ lat: 33.450701, lng: 126.570667 }}
+            style={{ width: '1200px', height: '600px', margin : '0 auto' }}
+            level={3}
+        />
     </Wrap>
 }
 
